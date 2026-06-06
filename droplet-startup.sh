b83104ef7607a69dd93ca0b20214ba516210d36c
tmp-etc <jne.etc@pm.me>
2026-06-06 17:44:36 +0300

    Update droplet-startup.sh

--- droplet-startup.sh
+++ droplet-startup.sh
@@ -2,11 +2,30 @@
 
 MYIP='1.1.1.1'
 
+# firewall shite
 ufw --force reset
 ufw allow from $MYIP to any port 22 proto tcp
 ufw enable
-apt-get update 
-apt-get install -y ranger caca-utils highlight atool w3m poppler-utils mediainfo bat fzf rg
+# docker shite
+sudo apt remove -y $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+# install all the shite
+sudo apt update
+apt-get install -y ranger caca-utils highlight atool w3m poppler-utils mediainfo bat fzf rg docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# UX shite
 useradd aaa
 echo "alias xforce='mkdir blabla && tar -x -C blabla -f'" >> /home/aaa/.bashrc
 echo "export EDITOR=nvim" >> /home/aaa/.bashrc
